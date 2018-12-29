ca6cbae21080ae586bf841b1255d5b61526febe1
arsuq <arsuq@ryzen.home>
2018-12-29 13:34:52 +0200

    ..

--- FormatText.cs
+++ FormatText.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
-using System.Linq;
 
 namespace TestRunner
 {
@@ -9,15 +7,14 @@ namespace TestRunner
 	{
 		public static string JoinLines(int leftSpaces, params string[] lines)
 		{
+			if (leftSpaces < 1) throw new ArgumentOutOfRangeException("leftSpaces");
+			if (lines == null || lines.Length < 1) return string.Empty;
+
 			var sb = new StringBuilder();
 			var pad = string.Empty;
-
-			if (leftSpaces > 0)
-			{
-				var lp = new char[leftSpaces];
-				for (int k = 0; k < lp.Length; k++) lp[k] = ' ';
-				pad = new string(lp);
-			}
+			var lp = new char[leftSpaces];
+			for (int k = 0; k < lp.Length; k++) lp[k] = ' ';
+			pad = new string(lp);
 
 			foreach (var l in lines)
 				sb.AppendLine(pad + l);
@@ -27,6 +24,8 @@ namespace TestRunner
 
 		public static string JoinLines(params string[] lines)
 		{
+			if (lines == null || lines.Length < 1) return string.Empty;
+
 			var sb = new StringBuilder();
 
 			foreach (var l in lines)
